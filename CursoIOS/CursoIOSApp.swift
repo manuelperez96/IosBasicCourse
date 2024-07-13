a7460ae0d94413c3bde2425784ebd6bc3dfa8fb5
Manuel <perez.soto.manuel@hotmail.com>
2024-07-13 18:49:40 +0200

    Initial Commit

---
+++ CursoIOS/CursoIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursoIOSApp.swift
+//  CursoIOS
+//
+//  Created by Manuel Perez on 13/7/24.
+//
+
+import SwiftUI
+
+@main
+struct CursoIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
