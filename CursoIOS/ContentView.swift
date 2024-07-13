d5809a8a50d43e63f8d1c9553a65fc8522fa7b21
Manuel <perez.soto.manuel@hotmail.com>
2024-07-13 19:14:21 +0200

    feat: add basic layout screen

--- CursoIOS/ContentView.swift
+++ CursoIOS/ContentView.swift
@@ -10,12 +10,33 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Rectangle().foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                Rectangle().foregroundColor(.orange)
+                Rectangle().foregroundColor(.yellow)
+                
+            }
+            .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+            Rectangle()
+                .frame(height: 100)
+                .foregroundColor(.orange)
+            HStack {
+                Circle().foregroundColor(.cyan)
+                Rectangle().foregroundColor(.black)
+                Circle().foregroundColor(.purple)
+            }
+            Rectangle()
+                .frame(height: 100)
+                .foregroundColor(.orange)
+            HStack {
+                Rectangle().foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                Rectangle().foregroundColor(.orange)
+                Rectangle().foregroundColor(.yellow)
+                
+            } .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
         }
-        .padding()
+        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
+        .background(.red)
     }
 }
 
